c0b2d1312ff8ba4776c5c04839b43bbf4fc04641
dylan.salazar <dylan.salazar@grupopromerica.com>
2026-04-02 21:29:18 -0600

    se elimina los proyectos

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -11,37 +11,6 @@ export function Project() {
       <h2>Mis Proyectos</h2>
       <div className="projects">
         
-        <ScrollAnimation animateIn="flipInX">
-          <div className="project">
-            <header>
-              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
-                <title>Folder</title>
-                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
-              </svg>
-              <div className="project-links">
-                <a href="https://github.com/CodeVinayak/Jupyter-Notebook/tree/main/Credit%20Risk%20Analysis" target="\_blank" rel="noreferrer">
-                  <img src={githubIcon} alt="Visit site" />
-                </a>
-                {/* <a href="https://github.com/CodeVinayak/Jupyter-Notebook/blob/main/Credit%20Risk%20Analysis/Credit_Risk_Analysis.ipynb" target="\_blank" rel="noreferrer">
-                  <img src={externalLink} alt="Visit site" />
-                </a> */}
-              </div>
-            </header>
-            <div className="body">
-              <h3>Proyecto de Análisis de Riesgo Crediticio</h3>
-              <p>
-                Desarrollé un modelo de clasificación de riesgo crediticio de alta precisión usando XGBoost, logrando 100% en precisión, recall y F1-score para predecir incumplimientos de préstamos. Implementé preprocesamiento riguroso de datos, ingeniería de características y ajuste de hiperparámetros sobre datos desbalanceados, empleando métodos de ensamble, validación cruzada y técnicas de interpretación de modelos.
-              </p>
-            </div>
-            <footer>
-              <ul className="tech-list">
-                <li>XGBoost</li>
-                <li>Pandas</li>
-                <li>Matplotlib</li>
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation>
 
       </div>
     </Container>
